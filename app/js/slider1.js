5ad24af9d77bae7c5c816559ca603afc1384a6c5
Alexander Ruzhickii <ruzhickiisasha@gmail.com>
2019-01-25 21:35:35 +0200

    adding 2 slick sliders(with bags on mobile version) and burger menu

---
+++ app/js/slider1.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    $('.partners').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true
+    });
+};
---
+++ app/js/slider2.js
@@ -0,0 +1,9 @@
+
+    $('.slider-section__wrapper').slick({
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        arrows: false,
+        cssEase: 'linear',
+        pauseOnHover: true
+    });
